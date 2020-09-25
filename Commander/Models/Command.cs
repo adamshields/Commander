750dfc57dc3ff4c3e9ad9a51d68ed4ab01012d46
Adam Shields <adamenote@gmail.com>
2020-09-25 12:46:31 -0400

    Updated Command Model to put non nullable fields and constraints

--- Commander/Models/Command.cs
+++ Commander/Models/Command.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,17 @@ namespace Commander
 {
     public class Command
     {
+        //[Key] - you can use [Key] but not required to use it - by default Id will take it and make it PK
         public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(500)]
         public string HowTo { get; set; }
+        
+        [Required]
         public string Line { get; set; }
+        
+        [Required]
         public string Platform { get; set; }
     }
 }
